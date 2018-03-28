27f8c9dd5108d05994b539c452fc6b732f4d77e2
Andrei Papuc <andreicarlopapuc@Andrejcps-MacBook.local>
2018-03-28 16:32:39 +0300

    creation of file

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+// import the class
 const SimpleWikiSearcher = require('./searcher');
+// create an object
 const searcher = new SimpleWikiSearcher();
+const fs = require('fs');
+// call search function and results return via callback
 
+var answerFile = fs.createWriteStream('context.txt', {
+    flags: 'w' // 'a' means appending (old data will be preserved)
+  }) 
 searcher.search('Pneumonia', function(results){
     var obtained = results;
     console.log('results:' +obtained);
+    
+    answerFile.write(obtained);
+    answerFile.end();
 });
